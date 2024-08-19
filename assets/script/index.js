7dc61f6b7977d686e66eb79651271719f7498573
Xeni-ya <147890566+Xeni-ya@users.noreply.github.com>
2024-08-19 18:44:31 +0300

    index.js

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,4 +3,4 @@ const calculateTotalPrice = (quantity = 2, price = 15000000) => {
 }
 calculateTotalPrice(2, 57000000);
 calculateTotalPrice();
-alert('Стоимость покупки: ${calculateTotalPrice} рублей');
+alert(`Стоимость покупки: ${calculateTotalPrice()} рублей`);
