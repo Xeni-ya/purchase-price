5245250cd86b47e6f9e5d421c3efbb05f95c90fb
Xeni-ya <budyuginaxeniya@gmail.com>
2024-10-15 19:58:42 +0300

    fix: fix function call

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,7 +3,7 @@ const calculateTotalPrice = (quantity = 2, price = 15000000) => {
   const formatPrice = totalСost.toLocaleString('ru-RU');
   alert(`Стоимость покупки: ${formatPrice} рублей`);
 }
-
+// Ожидание полной загрузки DOM
 document.addEventListener('DOMContentLoaded', () => {
   calculateTotalPrice();
 });
