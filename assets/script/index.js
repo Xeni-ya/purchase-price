b398b19a93ee37de9a47e222bdbb5baa797199f9
Xeni-ya <budyuginaxeniya@gmail.com>
2024-10-15 23:45:06 +0300

    fix: an attempt to display the purchase price on the page

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,9 +1,24 @@
+// const calculateTotalPrice = (quantity = 2, price = 15000000) => {
+//   const totalCost = quantity * price;
+//   const formatPrice = totalCost.toLocaleString('ru-RU');
+//   alert(`Стоимость покупки: ${formatPrice} рублей`);
+// }
+// // Получаем элементы DOM после загрузки страницы
+// document.addEventListener('DOMContentLoaded', () => {
+//   calculateTotalPrice();
+// });
+
 const calculateTotalPrice = (quantity = 2, price = 15000000) => {
-  const totalСost = quantity * price;
-  const formatPrice = totalСost.toLocaleString('ru-RU');
-  alert(`Стоимость покупки: ${formatPrice} рублей`);
+  const totalCost = quantity * price;
+  const formatPrice = totalCost.toLocaleString('ru-RU');
+
+  const divPurchasePrice = document.getElementById('purchase-price');
+
+  const newElement = document.createElement('p');
+  newElement.textContent = `Стоимость покупки: ${formatPrice} рублей`;
+
+  divPurchasePrice.append(newElement);
 }
-// Ожидание полной загрузки DOM
 document.addEventListener('DOMContentLoaded', () => {
   calculateTotalPrice();
 });
